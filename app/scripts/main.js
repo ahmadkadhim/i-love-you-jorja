917d2e057e6caac2271e83b868eaad1ed43fc14b
AhmadKadhim <akadhim13@gmail.com>
2015-02-11 05:18:16 -0500

    Got the hearts flying, the text looks great and responsive, the song autoplays, next up: timing everything to the song and adding the Happy Valentines text at the end

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+
+
+function fullWidthHearts() {
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	$('#hearts').height(h).width(w);
+}
+
+$(fullWidthHearts);
+$(window).resize(fullWidthHearts);
